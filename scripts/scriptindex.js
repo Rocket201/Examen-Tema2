30b28bc903b04fe1b57861d4a478fef78c317130
Rocket201 <alejandrocorroto99@gmail.com>
2023-10-30 09:03:09 +0100

    inicializo variables

---
+++ scripts/scriptindex.js
@@ -0,0 +1,21 @@
+/**
+ * Author: Alejandro González-Corroto Fernández
+ * Git HUb: 
+ */
+
+let saldo = 1000
+PIN_CORRECTO = 1234
+
+window.document.addEventListener("load",  )
+
+
+const depositar = document.addEventListener("depositarBtn")
+const retirar = document.addEventListener("depositarBtn")
+const transferir = document.addEventListener("depositarBtn")
+const cambiarcontraseña = document.addEventListener("depositarBtn")
+const salir = document.addEventListener("depositarBtn")
+
+
+function depositar(){
+
+}
